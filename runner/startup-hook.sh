5c01a1c386aaf08ac62832c58e3cc7f52c46fb0d
root <bvarjavand@gmail.com>
2024-08-09 14:50:35 +0000

    add runner

---
+++ runner/startup-hook.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/bvarjavand/determined-source-control.git
+cp -f determined-source-control/const.yaml const.yaml
+cp -f determined-source-control/model_def.py model_def.py
